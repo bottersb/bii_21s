6ccf601343815e1d6bb986d214a5d1e3b8500be5
bottersb <benjamin.ottersbach@gmx.de>
2021-06-09 21:44:03 +0200

    added scene template

---
+++ web/js/sketches/scene.js
@@ -0,0 +1,12 @@
+// basic scene template
+function SCENE(){
+
+	this.setup = function () {}
+	this.draw = function () {
+	}
+	this.enter = function () {}
+	this.resize = function () {}
+	this.setMgr = function (mgr) {
+		this.mgr = mgr;
+	}
+}
